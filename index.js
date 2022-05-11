91a4268fa27b05e101b16387b06310bf3c51ef8e
Ana Nicoletti <86062663+AnaNicoletti@users.noreply.github.com>
2022-05-11 21:19:47 +0000

    Adicionadas rotas de "empresa"

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 var express = require("express");
 var app = express();
-var { usuario } = require("./models");
+var { usuario, empresa } = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
 
-app.get("/", async function(req, res){
+//usuario
+app.get("/usuarios", async function(req, res){
   var resultado = await usuario.findAll();
   res.json(resultado);
 });
 
-app.post('/', async function(req, res){
+app.post('/usuarios', async function(req, res){
   var resultado = await usuario.create(req.body);
   res.json(resultado);
 });
 
-app.put('/:id', async function(req, res){
+app.put('/usuarios/:id', async function(req, res){
   var resultado = await usuario.update(
     {
       nome: req.body.nome,
@@ -31,7 +32,7 @@ app.put('/:id', async function(req, res){
   res.json(resultado);
 });
 
-app.delete('/:id', async function(req, res){
+app.delete('/usuarios/:id', async function(req, res){
 var resultado = await usuario.destroy(
     {
       where: {
@@ -41,8 +42,48 @@ var resultado = await usuario.destroy(
   res.json(resultado);
 });
 
-app.get('/:id', async function(req, res){
+app.get('/usuarios/:id', async function(req, res){
   var resultado = await usuario.findByPk(req.params.id);
+  res.json(resultado);
+});
+
+//empresa
+app.get("/empresas", async function(req, res){
+  var resultado = await empresa.findAll();
+  res.json(resultado);
+});
+
+app.post('/empresas', async function(req, res){
+  var resultado = await empresa.create(req.body);
+  res.json(resultado);
+});
+
+app.put('/empresas/:id', async function(req, res){
+  var resultado = await empresa.update(
+    {
+      nome: req.body.nome,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  res.json(resultado);
+});
+
+app.delete('/empresas/:id', async function(req, res){
+var resultado = await empresa.destroy(
+    {
+      where: {
+        id: req.params.id,
+      },
+    });
+  res.json(resultado);
+});
+
+app.get('/empresas/:id', async function(req, res){
+  var resultado = await empresa.findByPk(req.params.id);
   res.json(resultado);
 });
 
